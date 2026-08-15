fb6f75017727d715bcc2cb49ac3b8230dd9e6667
taro <iiinumail@yahoo.co.jp>
2026-08-15 15:16:39 +0900

    動画全画面を防ぐ

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,7 @@ function App() {
               ref={videoRef}
               src={videoUrl}
               controls
+              playsInline
               onLoadedMetadata={handleLoadedMetadata}
             />
           )}
